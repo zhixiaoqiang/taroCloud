a56d44fd488cc0e692855ca6721e40b47c326a57
誌小強 <oncwnuP0rnVMPHgiWqo-39AWDsN0@git.weixin.qq.com>
2020-01-17 15:51:49 +0800

    chore: 升级taro => 2.0

--- config/index.js
+++ config/index.js
@@ -24,53 +24,36 @@ const config = {
     '828': 1.81 / 2,
   },
   outputRoot: 'dist',
-  plugins: {
-    babel: {
-      sourceMap: true,
-      presets: [
-        [
-          'env',
-          {
-            modules: false,
-          },
-        ],
-      ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-class-properties',
-        'transform-object-rest-spread',
-      ],
-    },
+  babel: {
+    presets: [['env', { modules: false }]],
+    plugins: [
+      'transform-decorators-legacy',
+      'transform-class-properties',
+      'transform-object-rest-spread',
+      ['transform-runtime', {
+        'helpers': false,
+        'polyfill': false,
+        'regenerator': true,
+        'moduleName': 'babel-runtime',
+      }],
+    ],
   },
   defineConstants: {}, // 全局变量设置
   alias: {
     '@': path.resolve(__dirname, '..', 'src'),
   },
-  weapp: {
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
-          },
-        },
-        pxtransform: {
-          enable: true,
-          config: {},
-        },
-        url: {
-          enable: true,
-          config: {
-            limit: 10240, // 设定转换尺寸上限
-          },
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]',
-          },
+  mini: {
+    webpackChain (chain, webpack) {},
+    cssLoaderOption: {},
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {},
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 10240, // 设定转换尺寸上限
         },
       },
     },
@@ -78,20 +61,16 @@ const config = {
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
-          },
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]',
-          },
+    webpackChain (chain, webpack) {},
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+          browsers: [
+            'last 3 versions',
+            'Android >= 4.1',
+            'ios >= 8',
+          ],
         },
       },
     },
